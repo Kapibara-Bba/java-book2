60a48c2cd2972f95ce35101081bbc59f7b6706c6
Masaya Furukawa <kapibara.bba0924@gmail.com>
2021-04-02 20:46:25 +0900

    [Add]Kasa1

---
+++ Kasa1.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Kasa1 {
+  public static void main(String[] args) {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    try {
+      System.out.println("降水確率を入力してください。");
+      String line = reader.readLine();
+      int n = Integer.parseInt(line);
+      System.out.println("降水確率は" + n + "です");
+      if (n >= 50) {
+        System.out.println("傘を忘れずに。");
+      } else {
+        System.out.println("傘は入りません。");
+      }
+      System.out.println("いってらっしゃい。");
+    } catch (IOException e) {
+      System.out.println(e);
+    }
+  }
+}
