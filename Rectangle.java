bdec91a2e7dd1d09832dcd0667829d74b9202f4e
Masaya Furukawa <kapibara.bba0924@gmail.com>
2021-04-13 00:11:53 +0900

    [Fix]Rectangle

--- Rectangle.java
+++ Rectangle.java
@@ -1,6 +1,52 @@
+import org.w3c.dom.css.Rect;
+
 public class Rectangle {
   int width;
   int height;
+  int x;
+  int y;
+  final int INITIAL_WIDTH;
+  final int INITIAL_HEIGHT;
+
+  Rectangle() {
+    width = INITIAL_WIDTH;
+    height = INITIAL_HEIGHT;
+    x = 0;
+    y = 0;
+  }
+
+  Rectangle(int width, int height) {
+    this.width = width;
+    this.height = height;
+    this.x = 0;
+    this.y = 0;
+  }
+
+  Rectangle(int width, int height, int x, int y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+  }
+
+  public void setLocation(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public void setSize(int width, int height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  @Override
+  public String toString() {
+    return "[" + x + ", " + y + ", " + width + ", " + height + "]";
+  }
+
+  Rectangle ntersect(Rectangle r) {
+
+  }
 
   void setSize(int w, int h) {
     width = w;
