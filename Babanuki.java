c4530d5c06e7c304893b07eab8b32f99bfb75e35
Masaya Furukawa <kapibara.bba0924@gmail.com>
2021-04-18 01:07:59 +0900

    [Add]Babanuki

--- Babanuki.java
+++ Babanuki.java
@@ -13,33 +13,45 @@ public class Babanuki {
 	  int k = 0;
 	  for (String[] i : trump_card) {
 		  for (String j : i) {
-			  card[k++] = j;
+			  card[k] = j;
+			  k++;
 		  }
 	  }
 	  
 	  String[] player = new String[27];
 	  String[] Com = new String[27];
-	  int a = 1;
+	  int a = 2;
 	  int b = 0;
 	  int x = 0;
 	  while (true) {
-		  int randomInt = (int)(Math.random());
+		  int randomInt = (int)(Math.random() * 52);
 		  if (card[randomInt] == null) {
 			  continue;
 		  } else if (a % 2 == 0) {
 			  player[b] = card[randomInt];
-			  System.out.println(player[b]);
+			  System.out.print(player[b]);
+			  card[randomInt] = null;
 			  a++;
 			  b++;
-		  } else if (a % 2 == 0) {
+		  } else if (a % 2 == 1) {
 			  Com[x] = card[randomInt];
+			  card[randomInt] = null;
 			  a++;
 			  x++;
 		  }
-		  if (a == 52) {
+		  if (a == 54) {
 			  break;
 		  }
 	  }
-	  System.out.println(player);
+	  
+	  List<String> play = Arrays.asList(player);
+	  List<String> playerCard = new ArrayList<>(play);
+	  List<String> comp = Arrays.asList(Com);
+	  List<String> comCard = new ArrayList<>(comp);
+	  
+	  for (String p : playerCard) {
+		  String c = p.substring(p.length() - 1);
+		  
+	  }
   }
 }
