8a308b3be7a70df81114438a36be25fb0a3a1abe
Masaya Furukawa <kapibara.bba0924@gmail.com>
2021-04-24 20:55:23 +0900

    [Add]ArrayListTest

--- ArrayListTest5.java
+++ ArrayListTest5.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class ArrayListTest5 {
   public static void main(String[] args) {
     ArrayList<Integer> list = new ArrayList<Integer>();
@@ -7,6 +9,8 @@ public class ArrayListTest5 {
     list.add(18);
     list.add(77);
     list.add(54);
+    list.add(66);
+    list.add(99);
 
     for (int n : list) {
       System.out.println(n);
