a283291b220ea6445b6993161ae952b3f12343b2
Masaya Furukawa <kapibara.bba0924@gmail.com>
2021-06-10 23:21:07 +0900

    [Add]goodsInquiry

---
+++ goodsInquiry.java
@@ -0,0 +1,40 @@
+public class goodsInquiry {
+  public static void main(String[] args) {
+
+    List<String> goodsInqiryItem = new ArrayList<String>();
+    goodsInqiryItem.add("a");
+    goodsInqiryItem.add("b");
+    goodsInqiryItem.add("c");
+    goodsInqiryItem.add("d");
+    goodsInqiryItem.add("e");
+    goodsInqiryItem.add("f");
+    goodsInqiryItem.add("g");
+    goodsInqiryItem.add("h");
+    goodsInqiryItem.add("i");
+    goodsInqiryItem.add("j");
+    goodsInqiryItem.add("k");
+    goodsInqiryItem.add("l");
+    goodsInqiryItem.add("m");
+    goodsInqiryItem.add("n");
+    goodsInqiryItem.add("o");
+    goodsInqiryItem.add("p");
+    goodsInqiryItem.add("q");
+    goodsInqiryItem.add("r");
+    goodsInqiryItem.add("s");
+    goodsInqiryItem.add("t");
+    goodsInqiryItem.add("u");
+
+    List<String> goodsInqiryItem1 = new ArrayList<String>();
+    List<String> goodsInqiryItem2 = new ArrayList<String>();
+    List<String> goodsInqiryItem3 = new ArrayList<String>();
+
+    int count = 13;
+    int pages = (int)Math.ceil(count / 9);
+
+    for (int i = 0; i < pages; i++) {
+      
+    }
+
+
+  }
+}
