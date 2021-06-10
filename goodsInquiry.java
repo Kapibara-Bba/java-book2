b40058894332141392e871fbe8cad4b319272f81
Masaya Furukawa <kapibara.bba0924@gmail.com>
2021-06-10 23:51:54 +0900

    [Add]goodsInquiry

--- goodsInquiry.java
+++ goodsInquiry.java
@@ -1,40 +1,55 @@
-public class goodsInquiry {
-  public static void main(String[] args) {
+package point_exchange;
 
-    List<String> goodsInqiryItem = new ArrayList<String>();
-    goodsInqiryItem.add("a");
-    goodsInqiryItem.add("b");
-    goodsInqiryItem.add("c");
-    goodsInqiryItem.add("d");
-    goodsInqiryItem.add("e");
-    goodsInqiryItem.add("f");
-    goodsInqiryItem.add("g");
-    goodsInqiryItem.add("h");
-    goodsInqiryItem.add("i");
-    goodsInqiryItem.add("j");
-    goodsInqiryItem.add("k");
-    goodsInqiryItem.add("l");
-    goodsInqiryItem.add("m");
-    goodsInqiryItem.add("n");
-    goodsInqiryItem.add("o");
-    goodsInqiryItem.add("p");
-    goodsInqiryItem.add("q");
-    goodsInqiryItem.add("r");
-    goodsInqiryItem.add("s");
-    goodsInqiryItem.add("t");
-    goodsInqiryItem.add("u");
+import java.util.ArrayList;
+import java.util.List;
 
-    List<String> goodsInqiryItem1 = new ArrayList<String>();
-    List<String> goodsInqiryItem2 = new ArrayList<String>();
-    List<String> goodsInqiryItem3 = new ArrayList<String>();
+public class PointInquiry {
 
-    int count = 13;
-    int pages = (int)Math.ceil(count / 9);
+	public static void main(String[] args) {
 
-    for (int i = 0; i < pages; i++) {
-      
-    }
+	    List<String> goodsInquiryItem = new ArrayList<String>();
+	    goodsInquiryItem.add("a");
+	    goodsInquiryItem.add("b");
+	    goodsInquiryItem.add("c");
+	    goodsInquiryItem.add("d");
+	    goodsInquiryItem.add("e");
+	    goodsInquiryItem.add("f");
+	    goodsInquiryItem.add("g");
+	    goodsInquiryItem.add("h");
+	    goodsInquiryItem.add("i");
+	    goodsInquiryItem.add("j");
+	    goodsInquiryItem.add("k");
+	    goodsInquiryItem.add("l");
+	    goodsInquiryItem.add("m");
+	    goodsInquiryItem.add("n");
+	    goodsInquiryItem.add("o");
+	    goodsInquiryItem.add("p");
+	    goodsInquiryItem.add("q");
+	    goodsInquiryItem.add("r");
+	    goodsInquiryItem.add("s");
+	    goodsInquiryItem.add("t");
+	    goodsInquiryItem.add("u");
 
+	    List<String> goodsInquiryItem1 = new ArrayList<String>();
+	    List<String> goodsInquiryItem2 = new ArrayList<String>();
+	    List<String> goodsInquiryItem3 = new ArrayList<String>();
 
-  }
+	    int count = 13;
+	    int pages = (int)Math.ceil(count / 9);
+
+	    for (int i = 0; i < pages; i++) {
+	      int n = 9 * 1;
+	      for (int j = n; j < 3; j++) {
+	    	  goodsInquiryItem1.add(goodsInquiryItem.get(i));
+	    	  goodsInquiryItem2.add(goodsInquiryItem.get(i + 3));
+	    	  goodsInquiryItem3.add(goodsInquiryItem.get(i + 6));
+	      }
+	    }
+
+	    System.out.println(goodsInquiryItem1);
+	    System.out.println(goodsInquiryItem2);
+	    System.out.println(goodsInquiryItem3);
+	  }
+	
+	
 }
