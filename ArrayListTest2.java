08398e6c1a953617e53399aae634bf86372019ed
Masaya Furukawa <kapibara.bba0924@gmail.com>
2021-06-14 23:01:55 +0900

    [Add]List

--- ArrayListTest2.java
+++ ArrayListTest2.java
@@ -11,35 +11,11 @@ public class ArrayListTest2 {
     list.add("Diana");
     list.add("Elmo");
 
-    goodsInquiryItem.add("a");
-    goodsInquiryItem.add("b");
-    goodsInquiryItem.add("c");
-    goodsInquiryItem.add("d");
-    goodsInquiryItem.add("e");
-    goodsInquiryItem.add("f");
-    goodsInquiryItem.add("g");
-    goodsInquiryItem.add("h");
-    goodsInquiryItem.add("i");
-    goodsInquiryItem.add("j");
-    goodsInquiryItem.add("k");
-    goodsInquiryItem.add("l");
-    goodsInquiryItem.add("m");
-    goodsInquiryItem.add("n");
-    goodsInquiryItem.add("o");
-    goodsInquiryItem.add("p");
-    goodsInquiryItem.add("q");
-    goodsInquiryItem.add("r");
-    goodsInquiryItem.add("s");
-    goodsInquiryItem.add("t");
-    goodsInquiryItem.add("u");
-    goodsInquiryItem.add("v");
-    goodsInquiryItem.add("w");
-    goodsInquiryItem.add("x");
-    goodsInquiryItem.add("y");
-    goodsInquiryItem.add("z");
+    List<String> lists = new ArrayList<String>();
 
     for (int i = 0; i < list.size(); i++) {
-      System.out.println(list.get(i));
+      lists.add(list.get(i));
     }
+    System.out.println(lists);
   }
 }
