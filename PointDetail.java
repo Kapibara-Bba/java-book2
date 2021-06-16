a5729b416194b02fde21efe99bb07e0d64edc3ea
Masaya Furukawa <kapibara.bba0924@gmail.com>
2021-06-16 22:19:27 +0900

    [Add]pointDetail

---
+++ PointDetail.java
@@ -0,0 +1,25 @@
+public class PointDetail {
+  public static void main(String[] args) {
+
+    List<String> list = new ArrayList<String>();
+
+    goodsInquiryItem.add("a");
+    goodsInquiryItem.add("b");
+    goodsInquiryItem.add("c");
+    goodsInquiryItem.add("d");
+    goodsInquiryItem.add("e");
+    goodsInquiryItem.add("f");
+    goodsInquiryItem.add("g");
+    goodsInquiryItem.add("h");
+    goodsInquiryItem.add("i");
+    goodsInquiryItem.add("j");
+    goodsInquiryItem.add("k");
+    goodsInquiryItem.add("l");
+    goodsInquiryItem.add("m");
+    goodsInquiryItem.add("n");
+    goodsInquiryItem.add("o");
+    goodsInquiryItem.add("p");
+
+    
+  }
+}
