fbe49dedfb4aa001fcbdd3539a208051b333cad3
Masaya Furukawa <kapibara.bba0924@gmail.com>
2021-07-29 23:44:52 +0900

    [Add]java

---
+++ java2_2.java
@@ -0,0 +1,13 @@
+public class java2_2 {
+  class Main {
+    public static void main(String[] args) {
+      // 8と5を比較し、falseとなるようにしてください
+      System.out.println(8 < 5);
+      
+      // 3と2を比較し、trueとなるようにしてください
+      System.out.println(3 >= 2);
+      
+    }
+  }
+  
+}
