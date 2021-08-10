26b41ac1ff29c6ca85fa75892e5049b74e4104e8
Masaya Furukawa <kapibara.bba0924@gmail.com>
2021-08-10 23:50:41 +0900

    [Add]java

--- java2_10.java
+++ java2_10.java
@@ -6,19 +6,19 @@ class Main {
       // iが5の倍数のとき、繰り返し処理を終了してください
       if (i % 5 == 0) {
         break;
-      } 
-      
+      }
+
       System.out.println(i);
       i++;
     }
-    
+
     System.out.println("=== for文 ===");
     for (int j = 1; j < 10; j++) {
       // jが3の倍数のとき、処理をスキップしてください
       if (j % 3 == 0) {
         continue;
       }
-      
+
       System.out.println(j);
     }
   }
